1a463e5fbdea51139a8d923e04103d84eb8605e1
RONTheCookie <me@ronthecookie.me>
2019-06-24 13:30:29 +0300

    refactor for my purpose

---
+++ index.d.ts
@@ -0,0 +1,65 @@
+/* Declaration file wrriten by ronthecookie <ronthecookie0101@gmail.com>
+ * Need to finish!!!!
+ */
+import { Vec3 } from "vec3";
+import { Chunk } from "prismarine-chunk";
+import { Block } from "prismarine-block";
+export = prismarine_world;
+
+declare function prismarine_world(mcVersion: string): any;
+interface iniFunc {
+    (x: number, y: number, z: number);
+}
+interface chunkGeneratorFunc {
+    (chunkX: number, chunkZ: number);
+}
+declare class World {
+    constructor(chunkGenerator?, regionFolder?, savingInterval?: number);
+
+    initialize(
+        iniFunc: iniFunc,
+        length: number,
+        width: number,
+        height: number,
+        iniPos: Vec3
+    );
+    getColumns(): Chunk[];
+    getColumn(chunkX: number, chunkZ: number): Promise<Chunk>;
+
+    setColumn(chunkX: number, chunkZ: number, chunk: Chunk, save: boolean): Promise<void>;
+
+    startSaving(): void;
+
+    waitSaving(): Promise<void>;
+
+    stopSaving(): void;
+
+    queueSaving(chunkX: number, chunkZ: number): void;
+
+    saveAt(pos: Vec3): Promise<void>;
+
+    getColumnAt(pos: Vec3): Promise<Chunk>
+
+    setBlock(pos: Vec3, block: Block): Promise<void>;
+
+    getBlock(pos: Vec3): Promise<Block>;
+
+    getBlockType(pos: Vec3): Promise<number>;
+
+    getBlockData(pos: Vec3): Promise<object>;
+
+    getBlockLight(pos: Vec3): Promise<number>;
+
+    getSkyLight(pos: Vec3): Promise<number>;
+
+    getBiome(pos: Vec3): Promise<number>;
+
+    setBlockType(pos: Vec3, blockType: number): Promise<void>;
+    setBlockData(pos: Vec3, data: Object): Promise<void>;
+
+    setBlockLight(pos: Vec3, light: number): Promise<void>;
+
+    setSkyLight(pos: Vec3, light: number): Promise<void>;
+
+    setBiome(pos: Vec3, biome: number): Promise<void>;
+}
